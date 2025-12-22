98a6d5170148c4e6c231fb81498bf7059a4eb045
Márcio Júnior <marcio.mj70@gmail.com>
2025-12-21 21:24:03 -0300

    feat: implement user repository

---
+++ api/internal/repository/user_repo.go
@@ -0,0 +1,41 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/marcioecom/permit/internal/models"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, p *models.User) (string, error)
+	GetByID(ctx context.Context, id string) (*models.User, error)
+}
+
+type postgresUserRepo struct {
+	db *pgxpool.Pool
+}
+
+func NewPostgresUserRepo(db *pgxpool.Pool) UserRepository {
+	return &postgresUserRepo{db: db}
+}
+
+func (r *postgresUserRepo) Create(ctx context.Context, u *models.User) (string, error) {
+	var insertedUserID string
+	err := r.db.QueryRow(ctx, `
+		INSERT INTO users (id, email)
+		VALUES ($1, $2)
+		ON CONFLICT (email) DO UPDATE SET email = EXCLUDED.email
+		RETURNING id;
+		`,
+		u.ID, u.Email).Scan(&insertedUserID)
+	if err != nil {
+		return "", err
+	}
+
+	return insertedUserID, nil
+}
+
+func (r *postgresUserRepo) GetByID(ctx context.Context, id string) (*models.User, error) {
+	return nil, nil
+}
